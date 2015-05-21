14447c841feeba962042a3b948b54bdeaf37851c
Peter Marquardt <wwwutz@molgen.mpg.de>
2015-05-21 23:23:05 +0200

    finishing the title page
    that's the most important part. 8)

--- mklogo.pl
+++ mklogo.pl
@@ -9,11 +9,23 @@ my $dy = 256;
 my @TRACKS;
 open I,'<','tracks.data' or die "$?: $!";
 while (<I>) {
+    /^\-/ and next; # skip tracks
+    /^\s*$/ and next;
     chomp;
+    
     my ($f) = split(/\//,$_);
     push @TRACKS,$f;
 }
 close I;
+
+@TRACKS = sort { rand(1) > 0.5 } @TRACKS;
+
+for ( my $i=0; $i < @TRACKS; $i++) {
+  my $j = rand(@TRACKS);
+  my ($a,$b) = ( $TRACKS[$i], $TRACKS[$j] );
+  ( $TRACKS[$i], $TRACKS[$j] ) = ( $b, $a );
+}
+
 open O,'>','logos-fp.tex' or die "$?: $!";
 
 my $tp = 0;
@@ -44,7 +56,7 @@ sub incgra {
   my ($x, $y, $tp) = @_;
   print "### [$tp] $TRACKS[$tp] $x / $y\n";
   my $tx = int ($x * 0.1171875);
-  my $ty = int ($y * 0.1171875) + 150;
+  my $ty = int ($y * 0.1171875) + 0;
 #  print O '\begin{textblock*}{\Logo}('.$tx.'mm,'.$ty.'mm)%'."\n";
 #  print O '\includegraphics[width=\Logo]{LG/'.$TRACKS[$tp].'}'."\n";
   print O '\begin{textblock*}{30mm}('.$tx.'mm,'.$ty.'mm)%'."\n";
@@ -54,6 +66,30 @@ sub incgra {
 
 
 __DATA__
+xxxxxxx
+x.....x
+x.....x
+x.....x
+x.....x
+.......
+x.x.x.x
+.x.x.x.
+x.x.x.x
+.x.x.x.
+
+
+xxxxxxx
+x.....x
+x.....x
+x.....x
+x.....x
+x.....x
+x.....x
+x.....x
+x.....x
+xxx.xxx
+
+
 xxx.xxx
 xxxxxxx
 ..xxx..
