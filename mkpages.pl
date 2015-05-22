a8ef41098432377db340f05cad20e7d7bf9e0618
Peter Marquardt <wwwutz@molgen.mpg.de>
2015-05-22 15:41:00 +0200

    first release

--- mkpages.pl
+++ mkpages.pl
@@ -7,28 +7,39 @@ my $y = 0;
 my $dx = 256;
 my $dy = 256;
 
-my @LOCATIONS;
+# my @LOCATIONS;
 my $TRACKS;
+my $LOCATIONS;
 open I,'<','tracks.data' or die "$?: $!";
 my $loc = '';
+my $lc = 0;
 while (<I>) {
     /^\s*$/ and next;
     
     chomp;
     if (/^\-(.*)/) {
-      my ($f,$name,$type,$length,$turns,$year) = split(/\//,$1);
+      my ($f,$name,@r) = split(/\//,$1);
+      map { $TRACKS->{$loc}->{$name}->{$_} = shift @r } qw( type miles turns year );
       $TRACKS->{$loc}->{$name}{img} = $f;
+      my ($l) = $TRACKS->{$loc}->{$name}->{miles} =~ m/(\d+\.\d+)/;
+      $TRACKS->{$loc}->{$name}->{km} = sprintf("%0.2f",$l * 1.609344);
+      $TRACKS->{$loc}->{$name}->{mi} = sprintf("%0.2f",$l);
     }
     else {
       my ($f,$location,$country) = split(/\//,$_);
-      push @LOCATIONS,$f;
-      $loc = $f;
+      $LOCATIONS->{$location}->{img} = $f;
+      $LOCATIONS->{$location}->{country} = $country;
+      $lc++;
+      $loc = $location;
     }
 }
 close I;
 print Dumper(\$TRACKS);
 
-@LOCATIONS = sort { rand(1) > 0.5 } @LOCATIONS;
+# @LOCATIONS = sort { rand(1) > 0.5 } @LOCATIONS;
+my @LOCATIONS = sort { rand(1) > 0.5 } keys %{$LOCATIONS};
+
+print Dumper(\$LOCATIONS);
 print Dumper(\@LOCATIONS);
 
 for ( my $i=0; $i < @LOCATIONS; $i++) {
@@ -36,6 +47,8 @@ for ( my $i=0; $i < @LOCATIONS; $i++) {
   my ($a,$b) = ( $LOCATIONS[$i], $LOCATIONS[$j] );
   ( $LOCATIONS[$i], $LOCATIONS[$j] ) = ( $b, $a );
 }
+print "XXX\n";
+print Dumper(\@LOCATIONS);
 
 open O,'>','logos-fp.tex' or die "$?: $!";
 
@@ -62,14 +75,44 @@ close O;
 #
 # pages
 #
-
+my %TYPEMAP = (
+ 'C' => '\Huge$\circlearrowleft$',
+ 'P' => 'A $\rightsquigarrow$ B',
+);
 open O,'>','all-tracks.tex' or die "$?:$!";
-for my $loc (@LOCATIONS) {
-  for my $track ( keys %{$TRACKS->{$loc}} ) {
-    print O '\null\newpage'."\n";
-    print O '\begin{textblock*}{30mm}(0mm,0mm)%'."\n";
-    print O '\includegraphics[width=120mm]{TR/'.$TRACKS->{$loc}->{$track}->{img}."}\n";
-    print O '\end{textblock*}'."\n";
+my $r = 3;
+my $c = 0;
+my $bs = 80;
+my ($x1,$y1) = (15,15);
+my $printlogo = 1;
+for my $loc (sort @LOCATIONS) {
+  my $printlogo = 1;
+  for my $track ( sort keys %{$TRACKS->{$loc}} ) {
+    if (++$r >= 3 ) {
+      print O "\\null\\newpage\n\n";
+      $r = $c = 0;
+      $printlogo = 1;
+    }
+    if ($printlogo) {
+      $c = 0;
+      printf O "\\begin{textblock*}{%dmm}(%dmm,%dmm)%%\n",60, $x1 ,($y1 + $r * 90 );;
+      printf O "\\includegraphics[width=%dmm]{LG/%s}\n",50,$LOCATIONS->{$loc}->{img};
+      printf O "\\par %s\\\\ %s\n",$loc,$LOCATIONS->{$loc}->{country};
+      print  O "\\end{textblock*}\n";
+      $printlogo = 0;
+    }
+    $c = 1;
+    printf O "\\begin{textblock*}{%dmm}(%dmm,%dmm)%%\n",$bs,( $x1 + 70 ),($y1 + $r * 90 );
+    printf O "\\includegraphics[width=%dmm]{TR/%s}\n",$bs,$TRACKS->{$loc}->{$track}->{img};
+    printf O "\\centerline{%s}\n",$track;
+    print  O "\\end{textblock*}\n";
+    printf O "\\begin{textblock*}{%dmm}(%dmm,%dmm)%%\n",30,( $x1 + 70 + $bs +5 ),($y1 + $r * 90 );
+    printf O "\\par %s\n",$TYPEMAP{$TRACKS->{$loc}->{$track}->{type}};
+    print  O "\\Large\n";
+    printf O "\\par\$\\mapsto\$ %s mi.\n",$TRACKS->{$loc}->{$track}->{mi};
+    printf O "\\par\$\\mapsto\$ %s km\n",$TRACKS->{$loc}->{$track}->{km};
+    printf O "\\par\$\\looparrowright\$ %s\n",$TRACKS->{$loc}->{$track}->{turns};
+    print  O "\\end{textblock*}\n";
   }
 }
 close O;
@@ -80,13 +123,25 @@ sub incgra {
   print "### [$tp] $LOCATIONS[$tp] $x / $y\n";
   my $tx = int ($x * 0.1171875);
   my $ty = int ($y * 0.1171875) + 0;
-  print O '\begin{textblock*}{30mm}('.$tx.'mm,'.$ty.'mm)%'."\n";
-  print O '\includegraphics[width=30mm]{LG/'.$LOCATIONS[$tp].'}'."\n";
-  print O '\end{textblock*}'."\n";
+  printf O "\\begin{textblock*}{30mm}(%dmm,%dmm)%%\n",$tx,$ty;
+  printf O "\\includegraphics[width=30mm]{LG/%s}\n",$LOCATIONS->{$LOCATIONS[$tp]}->{img};
+  printf O "\\end{textblock*}\n";
 }
 
 
 __DATA__
+xxxxxxx
+x.....x
+x.....x
+x.....x
+x.....x
+.x.x.x.
+x.x.x.x
+.x.x.x.
+x.x.x.x
+
+
+
 xxxxxxx
 x.....x
 x.....x
